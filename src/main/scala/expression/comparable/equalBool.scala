b00fa99344867f5de41613e7f712c105ccb73d41
KomodoDile <35544206+KomodoDile@users.noreply.github.com>
2018-05-19 15:54:16 -0300

    Comparables Added

---
+++ src/main/scala/expression/comparable/equalBool.scala
@@ -0,0 +1,17 @@
+package expression
+package comparable
+
+import types._
+import value._
+
+/** Implementação da expressão de comparação "igual" para Booleans.
+ * @param 2 expressões de bool																																																																																																																														
+ * @return 1 valor bool (Value[TypeBool]) true caso expressão 1 seja igual a expressão 2, false em caso contrário
+ * @author Lucas V. M. Pinheiro
+ */
+
+case class EqualBool(_lhs: Expression[TypeBool], _rhs: Expression[TypeBool]) extends BinExpression[TypeBool, TypeBool, TypeBool](_lhs, _rhs) {
+
+	def eval[T1 >: TypeBool <: Type](): Value [T1] = Value[TypeBool](TypeBool(lhs.eval().innerValue() == rhs.eval().innerValue())).asInstanceOf[Value [T1]]
+
+}
